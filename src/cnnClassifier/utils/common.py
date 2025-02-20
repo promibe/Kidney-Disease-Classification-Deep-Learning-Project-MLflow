70844027339f59664e342ecfbb0123ca06f428b9
Promise Ibediogwu Ekele <pro9jaeduinfo@gmail.com>
2025-02-20 09:04:33 +0100

    utils added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -37,18 +37,18 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
     
 
-    @ensure_annotations
-    def create_directories(path_to_directories: list, verbose=True):
-        """create list of directories
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
 
-        Args:
-            path_to_directories (list): list of path of directories
-            ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
-        """
-        for path in path_to_directories:
-            os.makedirs(path, exist_ok=True)
-            if verbose:
-                logger.info(f"created directory at: {path}")
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
 
 
 
@@ -64,6 +64,7 @@ def save_json(path: Path, data: dict):
         json.dump(data, f, indent=4)
 
     logger.info(f"json file saved at: {path}")
+
 
 
 
